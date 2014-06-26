689b1248efee657f581a21138521057089c110c0
Nick Borromeo <ngborromeo@gmail.com>
2014-06-25 23:17:29 -0700

    figuring out how to mock out mechanize

--- spec/clipper_spec.rb
+++ spec/clipper_spec.rb
@@ -2,6 +2,13 @@ require_relative "spec_helper"
 require_relative "../lib/clipper_cli/clipper"
 
 describe ClipperCli::Clipper do
+  let(:clipper) { Clipper.new }
 
+  it 'should be able to get clipper login page' do
+    # uri = URI('https://www.clippercard.com/ClipperCard/needLogin.jsf')
+    # response = Net::HTTP.get(uri)
+
+    # expect(response).to be_an_instance_of(String)
+  end
 end
 
